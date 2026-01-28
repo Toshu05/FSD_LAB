4257df2f18947d14a29f56c52848fc1f6d2ec621
Toshu05 <toshupandey404@gmail.com>
2026-01-28 16:25:28 +0530

    Insert

---
+++ JAVASCRIPT.js
@@ -0,0 +1,4 @@
+console.log("Lappu sachin");
+document.getElementById("1").value;
+document.getElementById("1").value;
+sum=parseInt(document.getElementById("1").value)+parseInt(document.getElementById("1").value);
